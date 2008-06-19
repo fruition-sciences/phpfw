782127934e3bb238315c152e3c6cb2e64ec68817
yoniros <yoni@fruitionsciences.com>
2008-06-19 07:56:33 +0000

    Include process id in the log message.

--- classes/core/Logger.php
+++ classes/core/Logger.php
@@ -7,21 +7,26 @@
 
 class Logger {
     public static function info($msg) {
-        error_log("INFO: " . $msg);
+        self::writeMessage("INFO", $msg);
     }
 
     public static function error($msg, $exception=null) {
         if ($exception) {
             $msg .= "\n" . $exception->__toString();
         }
-        error_log("ERROR: " . $msg);
+        self::writeMessage("ERROR", $msg);
     }
 
     public static function debug($msg) {
-        error_log("DEBUG: " . $msg);
+        self::writeMessage("DEBUG", $msg);
     }
 
     public static function warning($msg) {
-        error_log("WARNING: " . $msg);
+        self::writeMessage("WARNING", $msg);
+    }
+    
+    private static function writeMessage($type, $msg) {
+        $pid = getmypid();
+        error_log("$type: (pid=$pid) " . $msg);
     }
 }
