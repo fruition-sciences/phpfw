057a7bd7766a98605beb9c05df6cc34d18c003ed
yoniros <yoni@fruitionsciences.com>
2008-12-07 00:01:16 +0000

    More flexible params to AutoConfig

--- tools/autoconfig/AutoConfig.php
+++ tools/autoconfig/AutoConfig.php
@@ -3,22 +3,62 @@
  * Created on Jan 18, 2008
  * Author: Yoni Rosenbaum
  *
+ * Processes a template file(s) by substituting variables with values defined
+ * in a properties file.
+ * The properties file is a php file which defines a global map named $props.
  */
 
 class AutoConfig {
     private $propertiesFile;
-    public function __construct($propertiesFile) {
-        $this->propertiesFile = $propertiesFile;
+    private $templateFile; // null if $templateDir is available
+    private $templateDir;  // null if $templateFile is available
+    private $outputDir;
+
+    public function parseArgs($args) {
+        while ($arg = next($args)) {
+            if ($arg == '-p') {
+                $this->propertiesFile = next($args);
+                if (!$this->propertiesFile) {
+                    return $this->printUsage("Missing value for -p argument");
+                }
+                continue;
+            }
+            if ($arg == '-tf') {
+                $this->templateFile = next($args);
+                if (!$this->templateFile) {
+                    return $this->printUsage("Missing value for -tf argument");
+                }
+                continue;
+            }
+            if ($arg == '-td') {
+                $this->templateDir = next($args);
+                if (!$this->templateDir) {
+                    return $this->printUsage("Missing value for -td argument");
+                }
+                continue;
+            }
+            if ($arg == '-o') {
+                $this->outputDir = next($args);
+                if (!$this->outputDir) {
+                    return $this->printUsage("Missing value for -o argument");
+                }
+                continue;
+            }
+            else {
+                return $this->printUsage("Invalid argument: $arg");
+            }
+        }
+        return true;
     }
 
-    public function processDir($templateFile, $outputDir) {
+    public function process() {
         include($this->propertiesFile);
-        $template = 'global $props; ?>' . file_get_contents($templateFile);
+        $template = 'global $props; ?>' . file_get_contents($this->templateFile);
         $processedTemplate = $this->processTemplate($template);
-        if (!file_exists($outputDir)) {
-            mkdir($outputDir, 0777, true);
+        if (!file_exists($this->outputDir)) {
+            mkdir($this->outputDir, 0777, true);
         }
-        $outFile = $outputDir . "/" . basename($templateFile);
+        $outFile = $this->outputDir . "/" . basename($this->templateFile);
         file_put_contents($outFile, $processedTemplate);
         print "Wrote file " . $outFile . "\n";
     }
@@ -30,13 +70,26 @@ class AutoConfig {
         ob_end_clean();
         return $buffer;
     }
+
+    public function printUsage($errorMessage) {
+        echo "AutoConfig: $errorMessage\n\n";
+        echo "AutoConfig: Processes a template file (or directory) using data from a properties file to\n" .
+                "produce a new file (or set of files).\n\n";
+        echo "Usage: AutoConfig.php [options]\n";
+        echo "Where options are:\n";
+        echo "  -p props_file : Properties file to use.\n";
+        echo "  -tf template_file : Template file. Cannot be used with the -td option.\n";
+        echo "  -td template_dir : Template directory. Cannot be used with the -tf option.\n";
+        echo "  -o output_dir : Output directory.\n";
+        return false;
+    }
 }
 
 // Main
-if ($argc != 4) {
-    echo "missing params\n";
+$autoconfig = new AutoConfig();
+if ($autoconfig->parseArgs($argv)) {
+    $autoconfig->process();
+}
+else {
     exit(1);
 }
-
-$autoconfig = new AutoConfig($argv[1]);
-$autoconfig->processDir($argv[2], $argv[3]);
