a96b3bdb79c2fd7dc8f00d0e1db7ac142d2d5702
bsoufflet <benjamin@fruitionsciences.com>
2010-03-30 12:35:29 +0000

    Add 2 new functions to file utils to list the directories of a directory or the files of a directory (by file extension if wanted).

--- classes/utils/FileUtils.php
+++ classes/utils/FileUtils.php
@@ -19,4 +19,39 @@ class FileUtils {
         ob_end_clean();
         return $contents;
     }
+    
+    /**
+     * Retrieve a list of directories of a given directory
+     * @param String $directoryPath
+     * @return Array List of directories
+     */
+    function dir_list($directoryPath){
+        $l = array();
+        $files = array_diff(scandir($directoryPath),array('.','..'));
+        foreach($files as $f){
+            if(is_dir($directoryPath.'/'.$f))
+                $l[]=$f;
+        }
+        return $l;
+    }
+    
+    /**
+     * array of files without directories... optionally filtered by extension
+     * @param String $directoryPath
+     * @param String $x extension (ex "xml")
+     * @return Array
+     */
+    function file_list($directoryPath,$x=null){
+        $l = array();
+        $files = array_diff(scandir($directoryPath),array('.','..'));
+        foreach($files as $f){
+            if(is_file($directoryPath.'/'.$f)){
+                if($x && strtolower(pathinfo($directoryPath.'/'.$f, PATHINFO_EXTENSION)) != $x){
+                    continue;
+                }
+                $l[]=$f;
+            }
+        }
+        return $l;
+    }
 }
