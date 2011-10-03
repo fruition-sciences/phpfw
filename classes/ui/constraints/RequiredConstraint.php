aead4e7d9f2420cbb2e986cbebd9e080531cfd4f
bsoufflet <benjamin@fruitionsciences.com>
2011-10-03 12:43:33 +0000

    Add support for file upload fields in the required constraint.

--- classes/ui/constraints/RequiredConstraint.php
+++ classes/ui/constraints/RequiredConstraint.php
@@ -8,7 +8,13 @@
 class RequiredConstraint extends Constraint {
 
     public function doValidate($ctx) {
-        $value = $ctx->getRequest()->getString($this->getName(), '');
+        $value = $ctx->getRequest()->getString($this->getName(), null);
+        if($value === null){
+            $value = '';
+            if(isset($_FILES[$this->getName()]['name'])){
+                $value = $_FILES[$this->getName()]['name'];
+            }
+        }
         if ($value == '') {
             $msg = I18nUtil::lookupString('REQUIRE_CONSTRAINT_MSG');
             $msg->set('fieldName', $this->getLabel());
