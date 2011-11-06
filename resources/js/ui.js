a211516b530e0840ece92c6bc0457322dc0acb8d
yoniros <yoni@fruitionsciences.com>
2011-11-06 01:27:46 +0000

    Update from vmms

--- resources/js/ui.js
+++ resources/js/ui.js
@@ -1,13 +1,35 @@
-function button_click(url) {
+/**
+ * Event for clicking on a button. Opens the given URL.
+ * 
+ * @param url
+ * @param target (optional) if set, URL will be opened in a new window with this
+ *        name.
+ */
+function button_click(url, target) {
     url = normalizeUrl(url);
-    document.location = url;
+    if (target) {
+        window.open(url, target);
+    }
+    else {
+        document.location = url;
+    }
 }
 
-function button_submit(url) {
+/**
+ * Event for clicking on a button. Supmits the form.
+ * 
+ * @param url
+ * @param target (optional) if set, form will be submitted into a new window
+ *        with this name.
+ */
+function button_submit(url, target) {
     var form = document.getElementById("theForm");
     form.method = "post";
     url = normalizeUrl(url);
     form.action = url;
+    if (target) {
+        form.target = target;
+    }
     form.submit();
 }
 
@@ -94,6 +116,14 @@ function getFirstButtonChild(element) {
             return node;
         }
     }
+    // Same with 'img' tag. TODO: Change to look for any type.
+    var nodes = element.getElementsByTagName('img');
+    for (var i=0; i<nodes.length; i++) {
+        var node = nodes.item(i);
+        if (node.getAttribute('button')) {
+            return node;
+        }
+    }
     return null;
 }
 
@@ -130,29 +160,118 @@ function updateHiddenTimeField(fieldName){
  * @return
  */
 function updateSelectTimeFields(fieldName){
-	if(document.getElementById(fieldName)){
-		var time = document.getElementById(fieldName).value;
-		if(time == ""){
-			updateHiddenTimeField(fieldName);
-		}else{
-			var temp = new Array();
-			var temp2 = new Array();
-			temp = time.split(":");
-			temp2 = temp[1].split(" ");
-			if(temp2[0]<8){
-				temp2[0]="00";
-			}else if(temp2[0]<23){
-				temp2[0]="15";
-			}else if(temp2[0]<38){
-				temp2[0]="30";
-			}else{
-				temp2[0]="45";
-			}
-			document.getElementById("_"+fieldName+"_hour").value = temp[0];
-			document.getElementById("_"+fieldName+"_minute").value = temp2[0];
-			document.getElementById("_"+fieldName+"_pm").value= temp2[1];
-		}
-	}
+    if(document.getElementById(fieldName)){
+        var time = document.getElementById(fieldName).value;
+        if(time == ""){
+            updateHiddenTimeField(fieldName);
+        }else{
+            var temp = new Array();
+            var temp2 = new Array();
+            temp = time.split(":");
+            temp2 = temp[1].split(" ");
+            if(temp2[0]<8){
+                temp2[0]="00";
+            }else if(temp2[0]<23){
+                temp2[0]="15";
+            }else if(temp2[0]<38){
+                temp2[0]="30";
+            }else{
+                temp2[0]="45";
+            }
+            document.getElementById("_"+fieldName+"_hour").value = temp[0];
+            document.getElementById("_"+fieldName+"_minute").value = temp2[0];
+            document.getElementById("_"+fieldName+"_pm").value= temp2[1];
+        }
+    }
+}
+
+/**
+ * Removes blank spaces in front and at the end of the string
+ * 
+ * @param String
+ */
+function trim(stringToTrim) {
+    return stringToTrim.replace(/^\s+|\s+$/g,"");
+}
+
+/**
+ * Hides an element by id
+ * 
+ * @param ID
+ */
+function hideElement(id) {
+    if (document.getElementById) { // DOM3 = IE5, NS6
+        document.getElementById(id).style.display = 'none';
+    } else {
+        if (document.layers) { // Netscape 4
+            eval("document." + id + ".visibility = 'hidden'");
+        } else { // IE 4
+            eval("document.all." + id + ".style.visibility = 'hidden'");
+        }
+    }
+}
+
+/**
+ * Shows an element by id
+ * 
+ * @param ID
+ */
+function showElement(id) {
+    if (document.getElementById) { // DOM3 = IE5, NS6
+        document.getElementById(id).style.display = 'block';
+    } else {
+        if (document.layers) { // Netscape 4
+            eval("document." + id + ".visibility = 'visible'");
+        } else { // IE 4
+            eval("document.all." + id + ".style.visibility = 'visible'");
+        }
+    }
+} 
+
+function moveIt(obj, mvTop, mvLeft) {
+    obj.style.position = "absolute";
+    obj.style.top = mvTop + 'px';
+    obj.style.left = mvLeft + 'px';
+}
+
+
+var xMousePos = 0;
+var yMousePos = 0;
+
+function captureMousePosition(e) {
+    var posx = 0;
+    var posy = 0;
+    if (!e) var e = window.event;
+    if (e.pageX || e.pageY)     {
+        posx = e.pageX;
+        posy = e.pageY;
+    }
+    else if (e.clientX || e.clientY)    {
+        posx = e.clientX + document.body.scrollLeft
+            + document.documentElement.scrollLeft;
+        posy = e.clientY + document.body.scrollTop
+            + document.documentElement.scrollTop;
+    }
+    // posx and posy contain the mouse position relative to the document
+    xMousePos = posx;
+    yMousePos = posy;
+}
+function encode(value) {
+    value = value.replace(/\r\n/g,"\n");
+    var utftext = "";
+  for (var n = 0; n < value.length; n++) {
+    var c = value.charCodeAt(n);
+    if (c < 128) { utftext += String.fromCharCode(c); }
+    else if((c > 127) && (c < 2048)) {
+      utftext += String.fromCharCode((c >> 6) | 192);
+      utftext += String.fromCharCode((c & 63) | 128);
+    } else {
+      utftext += String.fromCharCode((c >> 12) | 224);
+      utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+      utftext += String.fromCharCode((c & 63) | 128);
+    }
+  }
+  return escape(utftext);
 }
 
 /**
@@ -163,27 +282,27 @@ function updateSelectTimeFields(fieldName){
  * @return Date object
  */
 function getDateObject(dateString,dateSeperator){
-	var curValue=dateString;
-	var sepChar=dateSeperator;
-	var curPos=0;
-	var cDate,cMonth,cYear;
+    var curValue=dateString;
+    var sepChar=dateSeperator;
+    var curPos=0;
+    var cDate,cMonth,cYear;
 
-	//extract month portion
-	curPos=dateString.indexOf(sepChar);
-	cMonth=dateString.substring(0,curPos);
-	
-	//extract day portion				
-	endPos=dateString.indexOf(sepChar,curPos+1);			
-	cDate=dateString.substring(curPos+1,endPos);
+    //extract month portion
+    curPos=dateString.indexOf(sepChar);
+    cMonth=dateString.substring(0,curPos);
+    
+    //extract day portion               
+    endPos=dateString.indexOf(sepChar,curPos+1);            
+    cDate=dateString.substring(curPos+1,endPos);
 
-	//extract year portion				
-	curPos=endPos;
-	endPos=curPos+3;			
-	cYear=curValue.substring(curPos+1,endPos);
-	if(cYear.length == 2){
-		cYear = "20" + cYear;
-	}
-	//Create Date Object
-	dtObject=new Date(cYear,cMonth-1,cDate);	
-	return dtObject;
+    //extract year portion              
+    curPos=endPos;
+    endPos=curPos+3;            
+    cYear=curValue.substring(curPos+1,endPos);
+    if(cYear.length == 2){
+        cYear = "20" + cYear;
+    }
+    //Create Date Object
+    dtObject=new Date(cYear,cMonth-1,cDate);    
+    return dtObject;
 }
