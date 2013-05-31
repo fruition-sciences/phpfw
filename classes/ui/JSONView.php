e5c7d462c44710a2d7fc276f7f9879375e57918c
bcoignet <bcoignet@fruitionsciences.com>
2013-05-31 15:53:14 +0000

    Added displayResponseImmediately to send a response to the client before the end of the current php script. It adds specifics headers to do this.
    Added displayContentTypeHeader to avoid sending json header because we have a bug with IE8/9. When using form.submit in ajax, IE will download the response as a file instead of proccessed it.

--- classes/ui/JSONView.php
+++ classes/ui/JSONView.php
@@ -8,13 +8,61 @@
 class JSONView extends BaseView {
     private $doc; // php structure
     
+    /**
+     * If true, it indicates that we want to send/display the json response
+     * immediately and before the end of the current php script.
+     * @var boolean
+     */
+    private $displayResponseImmediately = false;
+    
+    /**
+     * If true display the content-type header.
+     * Else, do not display it to resolve issue with IE8/9 when the request is an
+     * ajax form submit. In this case IE will display the JSON response as a text file
+     * instead of passing it to the JS script.
+     * 
+     * @var boolean
+     */
+    private $displayContentTypeHeader = true;
+    
     public function __construct($doc) {
         $this->doc = $doc;
     }
 
     public function render($ctx) {
-        header("Content-Type: application/json ; encoding=utf-8");
+        if ($this->displayResponseImmediately) {
+            header("Connection: close");
+            header("Content-Length: " . mb_strlen(json_encode($this->doc)));
+        } 
+        if ($this->displayContentTypeHeader) {
+            header("Content-Type: application/json ; encoding=utf-8");
+        }
         echo json_encode($this->doc);
+        if ($this->displayResponseImmediately) {
+            flush();
+        }
+    }
+    
+    /**
+     * the render will send the response to the client of the initial request 
+     * before the end of the script.
+     * This way we don't have to wait until a long script is finished.
+     * @example client requests a long php task -> server start the long job.
+     *          the script return immediatly to the client: ok I will do the work.
+     *          the client know its ok
+     *          the php script continue to process the long task.
+     * Warning: When used, it is not possible to send an other response to the client.
+     * So anything printed after this call will not be send to the client.
+     */
+    public function displayResponseImmediately() {
+        $this->displayResponseImmediately = true;
+    }
+    
+    /**
+     * Its avoid to display content-type header.
+     */
+    public function notDisplayContentTypeHeader() {
+        $this->displayContentTypeHeader = false;
     }
 
     protected function setDoc($doc) {
