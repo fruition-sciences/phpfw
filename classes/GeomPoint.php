f56353309e9a5d3c6d1638b870b874bee5c20670
yoniros <yoni@fruitionsciences.com>
2015-03-20 13:11:34 -0700

    refactor: cleanup parsing WKT. Keep x & y as numbers, not strings.

--- classes/GeomPoint.php
+++ classes/GeomPoint.php
@@ -1,5 +1,5 @@
 <?php
-/* 
+/*
  * Created on Nov 02, 2011
  * Author: Ugo Fillastre
  */
@@ -12,13 +12,13 @@
 class GeomPoint {
     const NORTHERN_HEMISPHERE = 1;
     const SOUTHERN_HEMISPHERE = 2;
-    
+
     /**
      * Longitude
      * @var Double
      */
     private $x;
-    
+
     /**
      * Latitude
      * @var Double
@@ -33,46 +33,51 @@ class GeomPoint {
 
     public static function fromXY($x, $y) {
         $point = new GeomPoint();
-        $point->x = $x;
-        $point->y = $y;
+        $point->x = (double)$x;
+        $point->y = (double)$y;
         return $point;
     }
 
+    /**
+     * Parse a string in format such as: POINT(-122.3340921148 38.421022632969)
+     *
+     * @param String $wkt
+     * @return NULL|GeomPoint
+     * @throws IllegalArgumentException if the given string cannot be parsed as a point.
+     */
     public static function fromWKT($wkt) {
-        if (empty($wkt)){
-            Logger::error("WKT cannot be null");
-            throw new IllegalArgumentException();
-        } else {
-            $tab = explode(" ",$wkt);
-            $point = new GeomPoint();
-            $point->x = substr($tab[0], 6);
-            $point->y = substr($tab[1], 0, -1);
-            return $point;
+        if (empty($wkt)) {
+            return null;
         }
+        $ret = preg_match('/^POINT\((.*) (.*)\)$/', $wkt, $matches);
+        if (!ret) {
+            throw new IllegalArgumentException("Invalid POINT format: $wkt");
+        }
+        return self::fromXY($matches[1], $matches[2]);
     }
 
-    /** 
+    /**
      * Return WKT of the point type
      * http://dev.mysql.com/doc/refman/5.0/en/gis-wkt-format.html
-     * 
+     *
      * @return String
      */
     public function toWKT() {
         return "POINT(" . $this->x . " " . $this->y . ")";
     }
-    
+
     public function __toString() {
         return $this->toWKT();
     }
-    
+
     public function getX() {
         return $this->x;
     }
-    
+
     public function getY() {
         return $this->y;
     }
-    
+
     public function getHemisphere(){
         if($this->getY() >= 0){
             return self::NORTHERN_HEMISPHERE;
