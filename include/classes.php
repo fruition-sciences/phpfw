75a900d3135be4db52753e71fc5413f5a8787ec5
mlboua <sidiki@fruitionsciences.com>
2015-07-30 17:19:43 +0200

    refactor(build): Load pear/Mail_mime with composer instead of installing PEAR

--- include/classes.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
- * Created on Dec 28, 2007
- * Author: Yoni Rosenbaum
- *
- * Only the php files which have code outside a Classneed to be listed here.
- */
-
-require_once("classes/utils/functions.php");
