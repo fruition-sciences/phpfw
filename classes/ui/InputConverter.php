5ce33ca42146be0d3fbb541050fab63ce6a96347
yoniros <yoni@fruitionsciences.com>
2015-07-31 09:56:01 -0700

    fix(geo): Handle string "null" as value for a polygon field.
    Programmers should not really pass the string "null" as a value for a
    polygon, but apparently they do. Fix code to treat it as null.

--- classes/ui/InputConverter.php
+++ classes/ui/InputConverter.php
@@ -3,13 +3,13 @@
  * Converts back and forth various object types into serialized values in a map.
  * The map is similar to the one used to keep Form values. The values are all
  * formatted strings. Certain data types are kept in more than a single key in
- * the map. (For example, measure is kept in two fields: value & unit).  
+ * the map. (For example, measure is kept in two fields: value & unit).
  *
  * The 'getter' methods convert from values in the map (formatted strings) to a
  * single value (or object).
  * The 'setter' methods convert from a single value and sets the proper key(s)
  * in the map.
- * 
+ *
  * Created on Feb 4, 2011
  * Author: Yoni Rosenbaum
  */
@@ -27,15 +27,15 @@ class InputConverter {
 
     /**
      * Get a date (unix timestamp) from the given map.
-     * 
+     *
      * Currently, parses the value associated with the key.
      * Parses the date using the IntlDateFormatter SHORT format, which accepts
      * year as either 2 or 4 digits.
-     * 
+     *
      * TODO: The map should contain 2 fields:
      * - $key : keeps the formatted date
-     * - $key__time: keeps the formatted time 
-     * 
+     * - $key__time: keeps the formatted time
+     *
      * @param $map
      * @param $key
      * @return unix timestamp
@@ -54,7 +54,7 @@ class InputConverter {
      * Uses 2 fields:
      * - $key : keeps the formatted date
      * - $key__time: keeps the formatted time
-     * 
+     *
      * NOTE: Currenty just set the formatted date
      *
      * @param $map
@@ -71,7 +71,7 @@ class InputConverter {
     /**
      * Get time value (number of seconds) from the given map.
      * The value in the map is assumed to be formatted as: "HH:MM:SS"
-     * 
+     *
      * @param $map
      * @param $key
      * @return long number of seconds
@@ -89,10 +89,10 @@ class InputConverter {
     /**
      * Sets the given number of seconds into the given map.
      * The value set will be formatted as relative time: "HH:MM:SS"
-     * 
+     *
      * @param $map
      * @param $key
-     * @param $value 
+     * @param $value
      */
     public function setTime(&$map, $key, $value) {
         $map[$key] = $this->formatter->secondsToTime($value);
@@ -102,7 +102,7 @@ class InputConverter {
      * Get a measure object.
      * The measure object is constructed using the value and the unit should
      * be available in the map.
-     * 
+     *
      * @param $map
      * @param $key
      * @return Zend_Measure_Abstract
@@ -134,13 +134,13 @@ class InputConverter {
         $map[$key . '__unit'] = get_class($measure) . '::' . $measure->getType();
         $map[$key . '__measure'] = $measure;
     }
-    
+
     public function getId($map, $key) {
         $value = $this->getValue($map, $key);
         if (!$value) {
             return null;
         }
-        return (int)$value; 
+        return (int)$value;
     }
 
     public function setId(&$map, $key, $value) {
@@ -152,7 +152,7 @@ class InputConverter {
         if (!isset($value) || $value === "" || $value === false) {
             return null;
         }
-        return (int)$this->formatter->getNumber($value, 0); 
+        return (int)$this->formatter->getNumber($value, 0);
     }
 
     public function setLong(&$map, $key, $value) {
@@ -164,7 +164,7 @@ class InputConverter {
         if (!isset($value) || $value === "" || $value === false) {
             return null;
         }
-        return (float)$this->formatter->getNumber($value); 
+        return (float)$this->formatter->getNumber($value);
     }
 
     public function setDouble(&$map, $key, $value, $digits=null) {
@@ -195,9 +195,9 @@ class InputConverter {
         $map[$key] = $value;
     }
 
-    /** 
+    /**
      * Set longitude and latitude values into the map
-     * 
+     *
      * @param $map
      * @param $key
      * @param GeomPoint $geomPoint
@@ -206,14 +206,14 @@ class InputConverter {
         if ($geomPoint) {
             $map[$key . "_X"] = $geomPoint->getX();
             $map[$key . "_Y"] = $geomPoint->getY();
-        }    
+        }
         $map[$key] = $geomPoint;
     }
-    
-    /** 
+
+    /**
      * Create a string (with WKT format) from longitude and latitude
      * WKT format for point type: POINT(X Y)
-     *  
+     *
      * @return GeomPoint|null if values are not set properly in the map.
      */
     public function getPoint($map, $key) {
@@ -224,7 +224,7 @@ class InputConverter {
         }
         return GeomPoint::fromXY($x, $y);;
     }
-    
+
     /**
      * @param $map
      * @param $key
@@ -233,19 +233,20 @@ class InputConverter {
     public function setPolygon(&$map, $key, $geomPolygon) {
         $map[$key] = $geomPolygon ? $geomPolygon->toWKT() : null;
     }
-    
+
     /**
-     * 
+     *
      * Return a GeomPoint if the Block Geometry is specified in the map
      * Otherwise return null
-     * 
+     *
      * @param $map
      * @param $key
      * @return GeomPolygon|null
      */
     public function getPolygon($map, $key) {
         $wkt = $this->getValue($map, $key);
-        if (!empty($wkt)) {
+        // Also check for string "null". Apparently, some people submit it.
+        if (!empty($wkt) && $wkt != "null") {
             return new GeomPolygon($wkt);
         }
         return null;
