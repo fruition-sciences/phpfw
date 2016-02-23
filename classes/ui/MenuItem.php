f0d3c24596c48d680f9e71be450657b50e37daf8
iglou <vberge@fruitionsciences.com>
2016-02-23 17:36:18 +0100

    improv(style): improve vmms menu

--- classes/ui/MenuItem.php
+++ classes/ui/MenuItem.php
@@ -9,6 +9,12 @@ class MenuItem {
     private $id;
     private $name;
     private $href;
+    private $onclick;
+    private $class;
+    private $content;
+    private $subcontent;
+    private $add;
+    private $display;
     private $adminOnly; // Can be seen only by admin users
     private $items = array();
     private $selected = false;
@@ -30,6 +36,12 @@ class MenuItem {
         $this->adminOnly = $xmlElement['adminOnly'];
         $this->name = $xmlElement->name;
         $this->href = $xmlElement->href;
+        $this->onclick = $xmlElement->onclick;
+        $this->class = $xmlElement->class;
+        $this->content = $xmlElement->content;
+        $this->subcontent = $xmlElement->subcontent;
+        $this->add = $xmlElement->add;
+        $this->display = $xmlElement->disp;
         $this->xmlElement = $xmlElement;
         foreach ($xmlElement->item as $childItem) {
             $item = new MenuItem($this->menu, $childItem);
@@ -58,6 +70,48 @@ class MenuItem {
 
     public function getHref() {
         return (string)$this->href;
+    }
+    
+    public function getOnClick() {
+        if ($this->onclick) {
+            return (string)$this->onclick;
+        }
+        return null;
+    }
+    
+    public function getClass() {
+        if ($this->class) {
+            return (string)$this->class;
+        }
+        return null;
+    }
+    
+    public function getContent() {
+        if ($this->content) {
+            return (string)$this->content;
+        }
+        return null;
+    }
+    
+    public function getSubContent() {
+        if ($this->subcontent) {
+            return (string)$this->subcontent;
+        }
+        return null;
+    }
+    
+    public function getAdd() {
+        if ($this->add) {
+            return (string)$this->add;
+        }
+        return null;
+    }
+    
+    public function getDisplay() {
+        if ($this->display) {
+            return (string)$this->display;
+        }
+        return null;
     }
 
     public function getItems() {
