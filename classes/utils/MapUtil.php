3103c29262ea5e39a83175b2609aaf4a7cd0a284
yoniros <yoni@fruitionsciences.com>
2016-05-06 17:04:42 -0700

    Added method: groupBy

--- classes/utils/MapUtil.php
+++ classes/utils/MapUtil.php
@@ -20,4 +20,33 @@ class MapUtil {
         }
         return $default;
     }
+
+    /**
+     * Group (split) the given list of objects by their given method.
+     * Each item in the list is assumed to be an object which has a method with
+     * the given name.
+     * The result is a map (associative array) where the key is value returned
+     * by the method and the value is an array of objects.
+     *
+     * If the optional $valueCallback function is provided, then the items in
+     * each array in the returned map would be the result of a call to this
+     * function (with each item as parameter), rather than the items themselves.
+     *
+     * @param Object[] $list
+     * @param String $methodName
+     * @param function $valueCallback (optional)
+     */
+    public static function groupBy($list, $methodName, $valueCallback=null) {
+        $map = array();
+        foreach ($list as $item) {
+            $value = $valueCallback ? $valueCallback($item) : $item;
+            $key = call_user_func(array($item, $methodName));
+            if (isset($map[$key])) {
+                $map[$key][] = $value;
+            } else {
+                $map[$key] = array($value);
+            }
+        }
+        return $map;
+    }
 }
